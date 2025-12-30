6d3c3da33d7ff7781ce7d1083d55194aa0acd477
NupeDev <cruji42@gmail.com>
2025-12-29 19:43:52 -0600

    More lorem

--- src/app/demo/home1/components/working1/working1.component.ts
+++ src/app/demo/home1/components/working1/working1.component.ts
@@ -8,9 +8,9 @@ import { Component } from '@angular/core';
 })
 export class Working1Component {
   workingProcess = [
-    { icon: 'assets/img/icon/ca-work-1.1.svg', title: 'Coordination & Execution', link: 'service-single.html', description: 'We then coordinate & manage all aspects of transportation, leveraging our global network.' },
-    { icon: 'assets/img/icon/ca-work-1.2.svg', title: 'Planning and Strategy', link: 'service-single.html', description: 'Throughout the journey, our team monitors progress in real-time, providing you with updates.' },
-    { icon: 'assets/img/icon/ca-work1.3.svg', title: 'Real-Time Monitoring', link: 'service-single.html', description: 'Finally, we ensure timely and secure delivery of your goods, followed by a thorough review.' },
-    { icon: 'assets/img/icon/ca-work1.4.svg', title: 'Secure Transportation', link: 'service-single.html', description: 'Trust our meticulous process to streamline your supply chain and drive your business forward.' }
+    { icon: 'assets/img/icon/ca-work-1.1.svg', title: 'Especializado en análisis de alimentos e ingredientes (nutrición animal).', link: 'service-single.html', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { icon: 'assets/img/icon/ca-work-1.2.svg', title: 'Equipo moderno (NIRS, UPLC, etc.).', link: 'service-single.html', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { icon: 'assets/img/icon/ca-work1.3.svg', title: 'Confiable (Acreditaciones y aprobaciones EMA, SAGARPA, Cofepris, Ring test europeo).', link: 'service-single.html', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' },
+    { icon: 'assets/img/icon/ca-work1.4.svg', title: 'Alta velocidad de respuesta y sistema de consulta de resultados “On Line” las 24 horas del día.', link: 'service-single.html', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }
   ];
 }
