fb0d52f279a7ba2847604a25a4cba078cc2b2139
JulietGM <julieta.gam96@gmail.com>
2026-08-12 00:53:19 -0600

    fixed

--- src/app/views/blogs/blog/blog.component.ts
+++ src/app/views/blogs/blog/blog.component.ts
@@ -10,7 +10,53 @@ import { NewsPostCard } from '../../../models/news.model';
   selector: 'app-blog',
   imports: [BreadcrumbComponent, RouterLink, DatePipe, TranslatePipe],
   templateUrl: './blog.component.html',
-  styles: ``
+  styles: [`
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .row > [class*="col-"] {
+    display: flex;
+  }
+
+  .ca-team-box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+
+  .ca-team-img {
+    position: relative;
+    flex-shrink: 0;
+  }
+
+  .ca-blog-box-content {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+
+  .ca-blog-box-content p {
+    flex: 1;
+  }
+
+  /* Fix: la fecha debe quedar sobre la imagen, no sobre el meta de abajo */
+  .ca-blog-date-3 {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    bottom: auto;
+    z-index: 2;
+  }
+
+  .ca-b-meta {
+    position: relative;
+    z-index: 1;
+  }
+`]
 })
 export class BlogComponent implements OnInit {
   posts: NewsPostCard[] = [];
