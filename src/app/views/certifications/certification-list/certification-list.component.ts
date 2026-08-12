d60b7bc76df4b579caed76a0cf4683af65e08027
JulietGM <julieta.gam96@gmail.com>
2026-08-12 03:40:07 -0600

    fixed

--- src/app/views/certifications/certification-list/certification-list.component.ts
+++ src/app/views/certifications/certification-list/certification-list.component.ts
@@ -10,7 +10,53 @@ import { CertificationCard } from '../../../models/certification.model';
   selector: 'app-certification-list',
   imports: [CommonModule, RouterLink, DatePipe, BreadcrumbComponent, TranslatePipe],
   templateUrl: './certification-list.component.html',
-  styles: ``
+  styles: [`
+  .row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+ 
+  .row > [class*="col-"] {
+    display: flex;
+  }
+ 
+  .ca-team-box {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+ 
+  .ca-team-img {
+    position: relative;
+    flex-shrink: 0;
+  }
+ 
+  .ca-blog-box-content {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+  }
+ 
+  .ca-blog-box-content p {
+    flex: 1;
+  }
+ 
+  /* Fix: el tipo de certificación debe quedar sobre la imagen, no sobre el meta de abajo */
+  .ca-blog-date-3 {
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    bottom: auto;
+    z-index: 2;
+  }
+ 
+  .ca-b-meta {
+    position: relative;
+    z-index: 1;
+  }
+`]
 })
 export class CertificationListComponent implements OnInit {
   certifications: CertificationCard[] = [];
